31cc3955498b2327a0b9f8a410a7304bfddb2103
vinay <vinaynagisetty7@gmail.com>
2024-11-25 22:32:20 +0530

    intialliged the spring project

---
+++ springbootlearnings/src/main/java/com/vinaynagisetty/springbootlearnings/SpringbootlearningsApplication.java
@@ -0,0 +1,13 @@
+package com.vinaynagisetty.springbootlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlearningsApplication.class, args);
+	}
+
+}
---
+++ springbootlearnings/src/test/java/com/vinaynagisetty/springbootlearnings/SpringbootlearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinaynagisetty.springbootlearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
